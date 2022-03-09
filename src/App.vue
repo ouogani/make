7cd4764d9f35724b532009264e767336382ef44f
ouogani <ouogani@gmail.com>
2022-03-10 01:06:30 +0900

    headpost

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
 <div class="nav-frame">
-  <nav class="container">
-      <img src = "https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png">
-  </nav>
-</div> 
-<hr>
-  <main>
-    <Post/>
-  </main>
+  <div class="container">
+    <ul class="left">
+      <img src = "https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" class="logo">
+    </ul>
+    <ul class="right">
+      아이콘들
+    </ul>
+      <div class="search">
+        <div>돋</div>
+        <input placeholder="검색">
+      </div>
+    </div>
+</div>
+
+<Post/>
+ 
 </template>
 
 <script>
@@ -24,10 +32,20 @@ export default {
 
 @import 'style.css';
 
+#app {
+  box-sizing: border-box;
+  width: 100%;
+}
+
 body {
   background-color: white;
   height: 100vh;
   margin: 0;
+}
+
+ul {
+  padding: 5px;
+  list-style-type: none;
 }
 
 
--- src/Post.vue
+++ src/Post.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-        내용
+    <div class="post-box">
+           <div class="user">
+            <div class = "user-photo">
+                <img src="https://cdn.pixabay.com/photo/2017/11/13/07/14/cats-eyes-2944820_960_720.jpg" width="30" height="30px">
+            </div>
+            <div class="user-name">
+                name_gaeun
+            </div>
+            </div>
+        
+        <div class="post-img">
+            <img src="https://emle.org/wp-content/uploads/revslider/blog/demo-img-2.png" width="600px" height="500px">
+        </div> 
     </div>
 </template>
 
@@ -18,5 +29,33 @@ export default {
 
 @import 'style.css';
 
+.user{
+    display: flex;
+    align-items: center;
+    height: 60px;
+    width: 100%;
+    padding: 10px;
+   
+}
+
+.user-photo>img{
+    height: 30px;
+    width: 30px;
+    border: solid;
+    border-color: fuchsia;
+    padding: 1px;
+    border-radius: 50px;
+    margin-right: 10px;
+    align-items: center;
+}
+
+.post-box{
+    margin-left: 10px;
+    border-bottom: 1px solid #eee;
+    border-top: 1px solid #eee;
+    border-left: 1px solid #eee;
+    border-right: 1px solid #eee;
+    width: 602px;
+}
 
 </style>
